0c8369ed4ebbf3d51228eaaee64b8d5d0e3c7018
Eduardo Pacheco <eduardo@argmaxinc.com>
2025-11-21 13:58:22 -0300

    fix: format

--- src/openbench/metric/word_error_metrics/__init__.py
+++ src/openbench/metric/word_error_metrics/__init__.py
@@ -4,7 +4,7 @@
 from .english_abbreviations import ABBR
 from .text_normalizer import EnglishTextNormalizer
 from .word_error_metrics import (
-    WordErrorRate,
-    WordDiarizationErrorRate,
     ConcatenatedMinimumPermutationWER,
+    WordDiarizationErrorRate,
+    WordErrorRate,
 )
